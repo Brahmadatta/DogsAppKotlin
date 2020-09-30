411231205c833c81465de743dca1a6cf4223c30e
brahmadatta <g.brahmadatta@gmail.com>
2020-09-30 23:56:46 +0530

    added navigation controllers

---
+++ app/src/main/java/com/example/dogsappkotlin/view/DetailFragment.kt
@@ -0,0 +1,28 @@
+package com.example.dogsappkotlin.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.dogsappkotlin.R
+
+class DetailFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val action = DetailFragmentDirections.actionListFragment()
+//        Navigation.findNavController(id).navigate(action)
+    }
+}
