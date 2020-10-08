ae6869732a6003f2df5db274a42e559248f6b243
brahmadatta <g.brahmadatta@gmail.com>
2020-10-09 00:05:47 +0530

    added preferences with particular amount of cache time

--- app/src/main/java/com/example/dogsappkotlin/view/ListFragment.kt
+++ app/src/main/java/com/example/dogsappkotlin/view/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.example.dogsappkotlin.view
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
@@ -23,6 +21,7 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setHasOptionsMenu(true)
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
@@ -105,6 +104,23 @@ class ListFragment : Fragment() {
                 }
             }
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.actionSettings -> {
+                view?.let {
+                    Navigation.findNavController(it).navigate(ListFragmentDirections.actionSettings())
+                }
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
